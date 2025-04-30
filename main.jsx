8dd01896d9f5cc78ce1ae0152ab19053beb9c527
rexxlaw <rexx_law@omninexus.io>
2025-04-30 17:26:47 +0800

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HeyGemWeb from "./index.jsx";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeyGemWeb />);
